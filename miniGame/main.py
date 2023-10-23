3c6bde8ac40a4880bffdfeef51846e67aa43562f
KLin123 <karen234765@gmail.com>
2023-10-23 16:48:25 -0500

    Added setting for student customization

--- miniGame/main.py
+++ miniGame/main.py
@@ -10,12 +10,16 @@ data.dropna()
 print(data)
 del data["Domain"]
 
+###### THIS IS GOING TO BE USER INPUT IN THE FRONT END ######
+###### THIS IS INPUT IS ONLY FOR TESTING PURPOSES ########
+setting = input("What setting would you like your game to be placed in?")
+
 competencies = data["Competency Code"] + " " + data["Competency Description"]
 competencies = dict.fromkeys(competencies)
 
 microCompetencies = data["Micro-competency"] + " " + data["Micro-competency Description"]
 microCompetencies.dropna(inplace=True)
-prompt = "You will create educational minigames based off the learning goals. The learning goals are what the student should learn by the end of the minigame. Each learning goal has a minigame. Each learning goal will have sub-goals. The student must have learned every sub-goal associated with it's learning goal to be considered having learned the learning goal. Each sub-goal must be covered in a minigame. The minigame for each learning goal must include each subgoal. Each minigame should have the learning goal code, a description, game mechanics, a game flow that goes through what the player does, and the sub-goals."
+prompt = "You will create educational minigames based off the learning goals. The minigame should have a " + setting + " setting. The learning goals are what the student should learn by the end of the minigame. Each learning goal has a minigame. Each learning goal will have sub-goals. The student must have learned every sub-goal associated with it's learning goal to be considered having learned the learning goal. Each sub-goal must be covered in a minigame. The minigame for each learning goal must include each subgoal. Each minigame should have the learning goal code, a description, game mechanics, a game flow that goes through what the player does, and the sub-goals."
 learningGoals = "The learning goals are: \n" + "\n".join(competencies.keys())
 print(microCompetencies)
 subGoals = str("\nThe sub-goals are: \n" + "\n").join(microCompetencies)
